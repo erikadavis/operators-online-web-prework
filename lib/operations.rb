a5b3350a106f1cf0272a20cfaf82f92d0b028ab6
Erika Davis <davis2erika@gmail.com>
2019-04-09 23:41:09 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,10 +1,10 @@
 def unsafe?(speed)
-  if speed > 60
-    "true"
-elsif speed < 40
-  "true"
+  if speed == > 60
+    "unsafe"
+elsif speed == < 40
+  "unsafe"
 else
-return "false"
+return "safe"
 end
 end
 
